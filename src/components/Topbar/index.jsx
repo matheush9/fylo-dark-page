562ac7020cb52c756636cb7b3dddcfbace92996c
Matheus <matheushenr941@gmail.com>
2022-04-03 12:53:32 -0300

    fix diretorio, topbar

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,7 +1,17 @@
 import styles from "./topbar.module.scss";
+import Image from "next/image";
 
 export default function Topbar() {
   return (
-    <div><h1>oi</h1></div>
+    <div className={styles.component}>
+      <div className={styles.logo}>
+        <img src="/images/logo.svg" alt="" />
+      </div>
+      <div className={styles.navbar}>
+        <p>Features</p>
+        <p>Team</p>
+        <p>Sign In</p>
+      </div>
+    </div>
   );
 }
