6995d99e91ad1e8cce727cb98e4140f9f347aec4
Matheus <matheushenr941@gmail.com>
2022-04-03 20:38:51 -0300

    testemonials

--- src/components/Testemonials/index.jsx
+++ src/components/Testemonials/index.jsx
@@ -1,1 +1,71 @@
 import styles from "./testemonials.module.scss";
+import Image from "next/image";
+
+export default function Testemonials() {
+  return (
+    <div className={styles.component}>
+      <div className={styles.quotes}>
+        <img src="/images/bg-quotes.png" alt="" />
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.comment}>
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <img src="/images/profile-1.jpg" alt="" />
+            </div>
+
+            <div className={styles.founder}>
+              <p>Satish Patel</p>
+              <p>Founder & CEO, Huddle</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.comment}>
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <img src="/images/profile-2.jpg" alt="" />
+            </div>
+
+            <div className={styles.founder}>
+              <p>Bruce McKenzie</p>
+              <p>Founder & CEO, Huddle</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.comment}>
+          <p>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </p>
+
+          <div className={styles.container}>
+            <div className={styles.profile}>
+              <img src="/images/profile-3.jpg" alt="" />
+            </div>
+
+            <div className={styles.founder}>
+              <p>Iva Boyd</p>
+              <p>Founder & CEO, Huddle</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ export default function Home() {
       <Hero />
       <Items />
       <StayProductive />
-      {/* 
       <Testemonials />
+      {/* 
       <EarlyAcess />
       <Footer /> */}
     </div>
